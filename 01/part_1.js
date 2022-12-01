00d0d2aaeea65918562983987f6fb6e26977dbc1
Biswajit <biswajitp145@gmail.com>
2022-12-01 22:28:27 +0100

    part-2 of day-1

--- 01/part_1.js
+++ 01/part_1.js
@@ -25,3 +25,8 @@ for (let i=0; i<=input.length; i++){
 var max_calorie = listofElvesMaxCalories.max()
 var index = listofElvesMaxCalories.indexOf(max_calorie);
 console.log("Elves-%d has maximum calories of %d", index, max_calorie)
+var top_three_elves = listofElvesMaxCalories.sort(function(a, b){
+	return a-b;
+}).reverse().slice(0, 3)
+console.log(top_three_elves)
+console.log("Calories carried by top three Elves is %d", top_three_elves.reduce((a,b) => a+b, 0))
