cd180dc3e30f43973d80d472eb19b862b5b62f2c
Biswajit <biswajitp145@gmail.com>
2022-12-01 21:49:00 +0100

    initial code

---
+++ 01/part_1.js
@@ -0,0 +1,27 @@
+var path = require('path');
+const fs = require('fs');
+
+const input = fs
+	.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
+	.toString()
+	.trim()
+	.split('\n')
+	.map((num) => parseInt(num, 10));
+
+Array.prototype.max = function(){
+	return Math.max.apply(null, this);
+}
+
+var listofElvesMaxCalories = []
+var tempArr = []
+for (let i=0; i<=input.length; i++){
+	if(isNaN(input[i])){
+		listofElvesMaxCalories.push(tempArr.max())
+		var tempArr = []
+	} else {
+		tempArr.push(input[i])
+	}
+}
+var max_calorie = listofElvesMaxCalories.max()
+var index = listofElvesMaxCalories.indexOf(max_calorie);
+console.log("Elves-%d has maximum calories of %d", index, max_calorie)
